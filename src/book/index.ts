b296bd1b5107c58d61c18736d62cc7b61602d8b1
John Banister <banisterious@gmail.com>
2026-03-14 07:24:57 -0700

    feat: Add book module barrel exports (#294)

---
+++ src/book/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Book / Narrative Compilation module
+ *
+ * Exports for the book builder feature (#294).
+ */
+
+export type {
+	BookChapterType,
+	BookOutputFormat,
+	ReportChapterConfig,
+	VisualTreeChapterConfig,
+	VaultNoteChapterConfig,
+	SectionDividerConfig,
+	BookChapterConfig,
+	BookChapter,
+	BookMetadata,
+	BookOutputOptions,
+	BookDefinition,
+	ChapterGenerationResult,
+	BookGenerationProgress,
+	BookGenerationResult,
+} from './types/book-types';
+
+export { BookGenerationService } from './services/book-generation-service';
+export { PdfBookRenderer } from './services/pdf-book-renderer';
+export { OdtBookRenderer } from './services/odt-book-renderer';
